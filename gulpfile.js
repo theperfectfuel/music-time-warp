2d1fe1cb2dadc51a937b4b99aeb424371eac6bdc
theperfectfuel <robert@theperfectfuel.com>
2016-02-06 09:58:31 -0800

    Basic structure mostly in place

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+// JavaScript linting task
+gulp.task('jshint', function() {
+  return gulp.src('js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Compile Sass task
+gulp.task('sass', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'));
+});
+
+// Minify index
+gulp.task('html', function() {
+  return gulp.src('index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('/'));
+});
+
+// JavaScript build task, removes whitespace and concatenates all files
+gulp.task('scripts', function() {
+  return browserify('js/main.js')
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+});
+
+// Styles build task, concatenates all the files
+gulp.task('styles', function() {
+  return gulp.src('css/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('css'));
+});
+
+// Image optimization task
+gulp.task('images', function() {
+  return gulp.src('img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('img'));
+});
+
+// Watch task
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', ['jshint']);
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default task
+gulp.task('default', ['jshint', 'sass', 'watch']);
+
+// Build task
+gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images']);
---
+++ js/maps.js
@@ -0,0 +1,55 @@
+
+
+
+  var bittersMap = (function () {
+    var myLatlng = new google.maps.LatLng(40.072069, -100.5534),
+        mapCenter = new google.maps.LatLng(40.072069, -100.5534),
+        mapCanvas = document.getElementById('map_canvas'),
+        mapOptions = {
+          center: mapCenter,
+          zoom: 4,
+          scrollwheel: false,
+          draggable: true,
+          disableDefaultUI: true,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        },
+        map = new google.maps.Map(mapCanvas, mapOptions),
+        contentString =
+          '<div id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h1 id="firstHeading" class="firstHeading">thoughtbot</h1>'+
+          '<div id="bodyContent"'+
+          '<p>Sveavägen 98</p>'+
+          '</div>'+
+          '</div>',
+        infowindow = new google.maps.InfoWindow({
+          content: contentString,
+          maxWidth: 300
+        }),
+        marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'thoughtbot (Sweden)'
+        });
+
+    return {
+      init: function () {
+        map.set('styles', [{
+          featureType: 'landscape',
+          elementType: 'geometry',
+          stylers: [
+            { hue: '#ffff00' },
+            { saturation: 30 },
+            { lightness: 10}
+          ]}
+        ]);
+
+        google.maps.event.addListener(marker, 'click', function () {
+          infowindow.open(map,marker);
+        });
+      }
+    };
+  }());
+
+  bittersMap.init();
