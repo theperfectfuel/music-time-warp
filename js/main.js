68d5536d7c0e23f236330a7964b5b87ce47fc26d
theperfectfuel <robert@theperfectfuel.com>
2016-05-15 01:48:03 -0700

    Fixed indentation

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
 
-  var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+	var menuToggle = $('#js-mobile-menu').unbind();
+	$('#js-navigation-menu').removeClass("show");
 
 
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
-        $('#js-navigation-menu').removeAttr('style');
-      }
-    });
-  });
+	menuToggle.on('click', function(e) {
+		e.preventDefault();
+		$('#js-navigation-menu').slideToggle(function(){
+			if($('#js-navigation-menu').is(':hidden')) {
+				$('#js-navigation-menu').removeAttr('style');
+			}
+		});
+	});
 
 
-  $('#year-list').change(function() {
-    var year = $('#year').val();
+	$('#year-list').change(function() {
+		var year = $('#year').val();
 
-    var el = document.getElementById('year');
-    var text = el.options[el.selectedIndex].innerHTML;
-    $('#year-display').append('<div class="decades">The '+text+'</div>');
+		var el = document.getElementById('year');
+		var text = el.options[el.selectedIndex].innerHTML;
+		$('#year-display').append('<div class="decades">The '+text+'</div>');
 
-    getArtists(year);
-  });
+		getArtists(year);
+	});
 
-  $('#year-list').submit(function(e) {
-    e.preventDefault();
-    clearOverlays();
-  });
+	$('#year-list').submit(function(e) {
+		e.preventDefault();
+		clearOverlays();
+	});
 
 }); // close ready function
 
@@ -46,149 +46,150 @@ var center = {lat: 37.397, lng: -100.644};
 // MAP INITIALIZATION FUNCTION
 function initMap() {
 
-  // variable to style the google map
-  var styles = [
-    {
-      featureType: 'water',
-      elementType: 'geometry.fill',
-      stylers: [
-        {color: '#a1e6f7'},
-        //{color: '#E8CCC4'},
-        {invert_lightness: false}
-      ]
-    },{ 
-      featureType: 'water', 
-      elementType: 'labels', 
-      stylers: [ 
-        {invert_lightness: false}, 
-        {hue: '#ffffff'} 
-      ] 
-    },{
-      featureType: 'landscape.natural.landcover',
-      elementType: 'geometry.fill',
-      stylers: [
-        {hue: '#ddbcae'},
-        //{hue: '#EBDFDF'},
-        {lightness: 1}
-      ]
-    }
-  ];
+	// variable to style the google map
+	var styles = [
+		{
+			featureType: 'water',
+			elementType: 'geometry.fill',
+			stylers: [
+				{color: '#a1e6f7'},
+				//{color: '#E8CCC4'},
+				{invert_lightness: false}
+			]
+		},{ 
+			featureType: 'water', 
+			elementType: 'labels', 
+			stylers: [ 
+				{invert_lightness: false}, 
+				{hue: '#ffffff'} 
+			] 
+		},{
+			featureType: 'landscape.natural.landcover',
+			elementType: 'geometry.fill',
+			stylers: [
+				{hue: '#ddbcae'},
+				//{hue: '#EBDFDF'},
+				{lightness: 1}
+			]
+		}
+	];
 
-  // sets the center for roughly the center of the US
-  var bounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(25.82, -124.39),
-    new google.maps.LatLng(49.38, -66.94)
-  );
+	// sets the center for roughly the center of the US
+	var bounds = new google.maps.LatLngBounds(
+		new google.maps.LatLng(25.82, -124.39),
+		new google.maps.LatLng(49.38, -66.94)
+	);
 
-  var mapOptions = {
+	var mapOptions = {
 
-    mapTypeControlOptions: {
-      mapTypeIds: ['Styled']
-    },
-      bounds: bounds,
-      //center: {lat: 37.397, lng: -100.644},
-      //zoom: 4,
-      scrollwheel: true,
-      draggable: true,
-      disableDefaultUI: false,
-      mapTypeId: 'Styled'
-  };
+		mapTypeControlOptions: {
+			mapTypeIds: ['Styled']
+		},
+			bounds: bounds,
+			//center: {lat: 37.397, lng: -100.644},
+			//zoom: 4,
+			scrollwheel: true,
+			draggable: true,
+			disableDefaultUI: false,
+			mapTypeId: 'Styled'
+	};
 
-  var mapDiv = document.getElementById("map_canvas");
-  map = new google.maps.Map(mapDiv, mapOptions);
-  styledMapType = new google.maps.StyledMapType(styles, {name: 'Styled'});
-  map.mapTypes.set('Styled', styledMapType);
+	var mapDiv = document.getElementById("map_canvas");
+	map = new google.maps.Map(mapDiv, mapOptions);
+	styledMapType = new google.maps.StyledMapType(styles, {name: 'Styled'});
+	map.mapTypes.set('Styled', styledMapType);
 
-  map.fitBounds(bounds);
+	map.fitBounds(bounds);
 
-  mc = new MarkerClusterer(map);
-  geocoder = new google.maps.Geocoder();
+	mc = new MarkerClusterer(map);
+	geocoder = new google.maps.Geocoder();
 
 } // close initMap function
 
 
 function myAddMarkers(bandName, city, state, started) {
 
-  var bandInfo = "<div><p><strong>"+bandName+"</strong></p><p>"+city+", "+state+"</p><p>Started in: "+started+"</p></div>"
+	var bandInfo = "<div><p><strong>"+bandName+"</strong></p><p>"+city+", "+state+"</p><p>Started in: "+started+"</p></div>"
 
-  geocoder.geocode({address: city}, function (results, status) {
-    if(status == google.maps.GeocoderStatus.OK) {
-        var infowindow = new google.maps.InfoWindow({
-          content: bandInfo
-        });
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location,
-            animation: google.maps.Animation.DROP,
-            title: bandName
-        });
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-        markers.push(marker);
-    } // end of if
-  });
+	geocoder.geocode({address: city}, function (results, status) {
+		if(status == google.maps.GeocoderStatus.OK) {
+			var infowindow = new google.maps.InfoWindow({
+				content: bandInfo
+			});
+			var marker = new google.maps.Marker({
+				map: map,
+				position: results[0].geometry.location,
+				animation: google.maps.Animation.DROP,
+				title: bandName
+			});
+			marker.addListener('click', function() {
+				infowindow.open(map, marker);
+			});
+			markers.push(marker);
+		} // end of if
+	});
 
 } // end addMarker function
 
 // remove map markers and clear decades labels
 function clearOverlays() {
-  for (var i = 0; i < markers.length; i++ ) {
-    markers[i].setMap(null);
-  }
-  markers.length = 0;
-  $('.decades').remove();
+	for (var i = 0; i < markers.length; i++ ) {
+		markers[i].setMap(null);
+	}
+	markers.length = 0;
+	$('.decades').remove();
 }
 
 // ECHO NEST API CALL
 var getArtists = function(year) {
-  var startYear = year;
-  var endYear = (parseInt(year)+11);
-  endYear = endYear.toString();
+	var startYear = year;
+	var endYear = (parseInt(year)+11);
+	endYear = endYear.toString();
 
-  // the parameters we need to pass in our request to echonest's API
-  var request = { 
-    artist_start_year_after: startYear,
-    artist_start_year_before: endYear,
-  }; // end request
-  
-  var baseURL = "http://developer.echonest.com/api/v4/artist/search?api_key=DIVEWNESX3GN4Q7NV&";
-  var searchURL = "&artist_start_year_after=1969&artist_start_year_before=1980&";
-  var restofURL ="artist_location=us&bucket=artist_location&bucket=years_active&format=json&callback=?&results=30";
-  $.ajax({
-    url: baseURL+restofURL,
-    data: request,
-  }) // end ajax method
-  .done(function(response){ //this waits for the ajax to return with a succesful promise object
-    var artistResp = response.response.artists;
-    for (var i = 0; i < artistResp.length; i++) {
-        myFunction = function() {
+	// the parameters we need to pass in our request to echonest's API
+	var request = { 
+		artist_start_year_after: startYear,
+		artist_start_year_before: endYear,
+	}; // end request
+	
+	var baseURL = "http://developer.echonest.com/api/v4/artist/search?api_key=DIVEWNESX3GN4Q7NV&";
+	var searchURL = "&artist_start_year_after=1969&artist_start_year_before=1980&";
+	var restofURL ="artist_location=us&bucket=artist_location&bucket=years_active&format=json&callback=?&results=30";
+	$.ajax({
+		url: baseURL+restofURL,
+		data: request,
+	}) // end ajax method
+	.done(function(response){ //this waits for the ajax to return with a succesful promise object
+		var artistResp = response.response.artists;
+		for (var i = 0; i < artistResp.length; i++) {
+			myFunction = function() {
 
-          var artist = {
-            bandName: artistResp[i].name, 
-            city: artistResp[i].artist_location.city, 
-            state: artistResp[i].artist_location.region, 
-            started: artistResp[i].years_active[0].start};
+				var artist = {
+					bandName: artistResp[i].name, 
+					city: artistResp[i].artist_location.city, 
+					state: artistResp[i].artist_location.region, 
+					started: artistResp[i].years_active[0].start
+				};
 
-          myAddMarkers(artist.bandName, artist.city, artist.state, artist.started);
-          artistSet.push(artist);
-        };
-        myFunction();
-    } // end for loop
+				myAddMarkers(artist.bandName, artist.city, artist.state, artist.started);
+				artistSet.push(artist);
+			};
+			myFunction();
+		} // end for loop
 
-    $('#year').val('default');
+		$('#year').val('default');
 
-  }) // end done method
-  .fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
-    var errorElem = showError(error);
-    $('.search-results').append(errorElem);
-  }); // end fail method
+	}) // end done method
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	}); // end fail method
 }; // end getArtists
 
 
 // takes error string and turns it into displayable DOM element
 var showError = function(error){
-  var errorElem = $('.templates .error').clone();
-  var errorText = '<p>' + error + '</p>';
-  errorElem.append(errorText);
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
 };
--- js/maps.js
+++ js/maps.js
@@ -1,57 +1,56 @@
+var bittersMap = (function () {
+	var myLatlng = new google.maps.LatLng(40.072069, -100.5534),
+		mapCenter = new google.maps.LatLng(40.072069, -100.5534),
+		mapCanvas = document.getElementById('map_canvas'),
+		mapOptions = {
+			center: mapCenter,
+			zoom: 4,
+			scrollwheel: false,
+			draggable: true,
+			disableDefaultUI: true,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		},
+		map = new google.maps.Map(mapCanvas, mapOptions),
+		contentString =
+			'<div id="content">'+
+			'<div id="siteNotice">'+
+			'</div>'+
+			'<h1 id="firstHeading" class="firstHeading">thoughtbot</h1>'+
+			'<div id="bodyContent"'+
+			'<p>Sveavägen 98</p>'+
+			'</div>'+
+			'</div>',
+		infowindow = new google.maps.InfoWindow({
+			content: contentString,
+			maxWidth: 300
+		}),
+		marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			title: 'thoughtbot (Sweden)'
+		});
 
-  var bittersMap = (function () {
-    var myLatlng = new google.maps.LatLng(40.072069, -100.5534),
-        mapCenter = new google.maps.LatLng(40.072069, -100.5534),
-        mapCanvas = document.getElementById('map_canvas'),
-        mapOptions = {
-          center: mapCenter,
-          zoom: 4,
-          scrollwheel: false,
-          draggable: true,
-          disableDefaultUI: true,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        },
-        map = new google.maps.Map(mapCanvas, mapOptions),
-        contentString =
-          '<div id="content">'+
-          '<div id="siteNotice">'+
-          '</div>'+
-          '<h1 id="firstHeading" class="firstHeading">thoughtbot</h1>'+
-          '<div id="bodyContent"'+
-          '<p>Sveavägen 98</p>'+
-          '</div>'+
-          '</div>',
-        infowindow = new google.maps.InfoWindow({
-          content: contentString,
-          maxWidth: 300
-        }),
-        marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          title: 'thoughtbot (Sweden)'
-        });
+	return {
+		init: function () {
+			map.set('styles', [{
+				featureType: 'landscape',
+				elementType: 'geometry',
+				stylers: [
+					{ hue: '#ffff00' },
+					{ saturation: 30 },
+					{ lightness: 10}
+				]}
+			]);
 
-    return {
-      init: function () {
-        map.set('styles', [{
-          featureType: 'landscape',
-          elementType: 'geometry',
-          stylers: [
-            { hue: '#ffff00' },
-            { saturation: 30 },
-            { lightness: 10}
-          ]}
-        ]);
+			function printLatLng() {
+				console.log(myLatlng);
+			}
 
-        function printLatLng() {
-          console.log(myLatlng);
-        }
+			google.maps.event.addListener(marker, 'click', function () {
+				infowindow.open(map,marker);
+			});
+		}
+	};
+}());
 
-        google.maps.event.addListener(marker, 'click', function () {
-          infowindow.open(map,marker);
-        });
-      }
-    };
-  }());
-
-  bittersMap.init();
+bittersMap.init();
